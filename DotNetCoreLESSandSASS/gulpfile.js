59ccbd542159abbaeeb5d192641f20190b300469
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-08-10 20:47:32 -0400

    initial commit

---
+++ DotNetCoreLESSandSASS/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require("gulp"); 
+var cleanCss = require("gulp-clean-css");
+var less = require("gulp-less");
+
+gulp.task("default", function (){
+    return gulp.src("Styles/*.less")
+        .pipe(less())
+        .pipe(cleanCss({ compatibility: 'ie8' }))
+        .pipe(gulp.dest("wwwroot/css"));
+})
