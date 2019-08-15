8c6724cc5890f87799d051cfdcd09f807930f99d
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-08-14 21:57:29 -0400

    added equivalent SASS of LESS plus some extra funtionality

--- DotNetCoreLESSandSASS/gulpfile.js
+++ DotNetCoreLESSandSASS/gulpfile.js
@@ -1,10 +1,18 @@
 ﻿var gulp = require("gulp"); 
 var cleanCss = require("gulp-clean-css");
 var less = require("gulp-less");
+var sass = require("gulp-sass");
+//gulp.task("default", function (){
+//    return gulp.src("Styles/*.less")
+//        .pipe(less())
+//        .pipe(cleanCss({ compatibility: 'ie8' }))
+//        .pipe(gulp.dest("wwwroot/css"));
+//});
 
-gulp.task("default", function (){
-    return gulp.src("Styles/*.less")
-        .pipe(less())
+
+gulp.task("default", function () {
+    return gulp.src("Styles/*.scss")
+        .pipe(sass())
         .pipe(cleanCss({ compatibility: 'ie8' }))
         .pipe(gulp.dest("wwwroot/css"));
 })
